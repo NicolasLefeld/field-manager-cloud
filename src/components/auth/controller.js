4865b34769147e209841689b1c1b9f376111f853
Nicolas Lefeld <lefeldnicolas@gmail.com>
2021-09-22 11:20:49 -0300

    Better error messages

--- src/components/auth/controller.js
+++ src/components/auth/controller.js
@@ -7,39 +7,53 @@ const {
 } = require("firebase/auth");
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA6LxMvq9LxSK98GcFOgGFmtBj_qxyjrNo',
-  authDomain: 'fieldmanagerapp.firebaseapp.com',
-  projectId: 'fieldmanagerapp',
-  storageBucket: 'fieldmanagerapp.appspot.com',
-  messagingSenderId: '625593883192',
-  appId: '1:625593883192:web:76f73570d26219aeffa4cd',
-  measurementId: 'G-64R9TRVEHC',
+  apiKey: "AIzaSyA6LxMvq9LxSK98GcFOgGFmtBj_qxyjrNo",
+  authDomain: "fieldmanagerapp.firebaseapp.com",
+  projectId: "fieldmanagerapp",
+  storageBucket: "fieldmanagerapp.appspot.com",
+  messagingSenderId: "625593883192",
+  appId: "1:625593883192:web:76f73570d26219aeffa4cd",
+  measurementId: "G-64R9TRVEHC",
 };
 
 initializeApp(firebaseConfig);
 
 const userAndPasswordCreate = async (email, password) => {
-  const auth = await getAuth();
-  const { userCredential } = await createUserWithEmailAndPassword(
-    auth,
-    email,
-    password
-  );
+  try {
+    const auth = await getAuth();
+    const { userCredential } = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
 
-  return {
-    status: 200,
-    body: userCredential,
-  };
+    return {
+      status: 200,
+      body: userCredential,
+    };
+  } catch (error) {
+    return {
+      status: error.code,
+      body: error.message,
+    };
+  }
 };
 
 const userAndPasswordLogin = async (email, password) => {
-  const auth = getAuth();
-  const logedIn = await signInWithEmailAndPassword(auth, email, password);
+  try {
+    const auth = getAuth();
+    const logedIn = await signInWithEmailAndPassword(auth, email, password);
 
-  return {
-    status: 200,
-    body: logedIn,
-  };
+    return {
+      status: 200,
+      body: logedIn,
+    };
+  } catch (error) {
+    return {
+      status: error.code,
+      body: error.message,
+    };
+  }
 };
 
 const googleLogin = async () => {
